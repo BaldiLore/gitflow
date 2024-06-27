38588d9b729b490f1231b27afafa530a01fc2039
Lorenzo Baldi <602624@sistinf.it>
2024-06-27 15:46:11 +0200

    feature2 changes

--- src/main/java/it/test/gitflow/GitflowApplication.java
+++ src/main/java/it/test/gitflow/GitflowApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitflowApplication {
 
 	// CREATO DEVELOP
-
+	// FEATURE 2
 	public static void main(String[] args) {
 		SpringApplication.run(GitflowApplication.class, args);
 	}
