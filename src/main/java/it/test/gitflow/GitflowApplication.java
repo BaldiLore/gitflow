9e2b107753f7112fe1302703b3d49b13d6f306e3
Lorenzo Baldi <602624@sistinf.it>
2024-06-27 15:39:52 +0200

    develop

--- src/main/java/it/test/gitflow/GitflowApplication.java
+++ src/main/java/it/test/gitflow/GitflowApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitflowApplication {
 
+	// CREATO DEVELOP
+
 	public static void main(String[] args) {
 		SpringApplication.run(GitflowApplication.class, args);
 	}
